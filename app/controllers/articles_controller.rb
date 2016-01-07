05e6a5d2711f748e4606acd5e459c8a7d2ce41b0
steveorsino <steveorsino@yahoo.com>
2016-01-07 15:28:57 +0000

    Add pagination to article listings and users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def edit
   end
   def create
-    debugger
+    # debugger
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
