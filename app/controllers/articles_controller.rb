b6d73da3c00681eca7870de7e7f36081a8975131
steveorsino <steveorsino@yahoo.com>
2016-01-08 19:48:03 +0000

    fixed new user create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
   def edit
   end
   def create
-    # debugger
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the New blog, #{@user.username}"
       redirect_to articles_path
