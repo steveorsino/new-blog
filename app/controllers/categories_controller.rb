6b0872d43b8c5bfd0ae4c0086b1600e0b63009b1
steveorsino <steveorsino@yahoo.com>
2016-01-12 14:58:20 +0000

    created new categories dropdown with admin function to create new

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -30,7 +30,7 @@ class CategoriesController < ApplicationController
 
   def require_admin
     if !logged_in? || (logged_in? and !current_user.admin?)
-      flash[:danger] = "Only admins can perform that function"
+      flash[:danger] = "Only admins can perform that action"
       redirect_to categories_path
     end
   end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = User.create(username: "jon", email: "jon@email.com", password: "password", admin: true)
+  end
+
     test "get new category form and create category" do
+      sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -11,6 +17,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
     test "invalid category submission results in failure" do
+      sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
